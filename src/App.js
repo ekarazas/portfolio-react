9510e3fee44b82a428a85f64304ebdb118cbe71c
ekarazas <karazas7@gmail.com>
2021-06-27 20:20:05 +0300

    installed normalize.css and particles animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import ParticleBackground from "./components/ParticleBackground";
 
 const App = () => {
   return (
     <>
-      <h1>Hello React</h1>
+      <ParticleBackground />
+      <div className="center-all">
+        <h1>Hello React</h1>
+      </div>
     </>
   );
 };
